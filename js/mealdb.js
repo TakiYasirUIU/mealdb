2040a0ef1d7b9c6382acd9ac19ecad5594bd42f5
Taki Yasir <95402895+TakiYasirUIU@users.noreply.github.com>
2022-03-02 22:25:58 +0600

    single display done

--- js/mealdb.js
+++ js/mealdb.js
@@ -1,7 +1,7 @@
 const searchFood = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value ;
-    console.log(searchText);
+    // console.log(searchText);
     searchField.value = '';
     const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
     // console.log(url)
@@ -14,11 +14,11 @@ const searchFood = () => {
 const displaySearchResult = meals => {
     const searchResult = document.getElementById('search-result');
     meals.forEach(meal =>{
-        console.log(meal);
+        // console.log(meal);
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML=`
-        <div class="card h-100">
+        <div onclick="loadMealDetail(${meal.idMeal});" class="card h-100">
             <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${meal.strMeal}</h5>
@@ -30,3 +30,32 @@ const displaySearchResult = meals => {
     })
 
 }
+
+// load meal detail 
+
+const loadMealDetail = mealId => {
+    // console.log(mealId);
+    const url =`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMealDetail(data.meals[0]));
+
+}
+
+// display single meal detail 
+ const displayMealDetail = meal => {
+     console.log(meal);
+    const mealDetails = document.getElementById('meal-details');
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+    <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">${meal.strMeal}</h5>
+        <p class="card-text">${meal.strInstructions.slice(0, 150)}</p>
+        <a href="${meal.strYoutube}" class="btn btn-primary">Go somewhere</a>
+    </div>
+    `;
+    mealDetails.appendChild(div);
+}
+ 
